dd0217f14e6288f56cf93c828706eb9f1040fec0
aesqe <aesqe@skyphe.org>
2013-02-24 23:02:58 +0100

    media library 3.5 enhancements

--- js/file-gallery-media.js
+++ js/file-gallery-media.js
@@ -1,73 +1,78 @@
-/*global console _ wp jQuery file_gallery_attach_nonce file_gallery*/
-/**/
-var wpMediaAttachmentFilters = wp.media.view.AttachmentFilters;
-wpMediaAttachmentFilters = wp.media.view.AttachmentFilters.extend(
+/*global console _ wp jQuery file_gallery_attach_nonce console file_gallery*/
+
+jQuery(document).ready(function()
 {
-	change: function( event )
+	"use strict";
+
+	var wpMediaFramePost = wp.media.view.MediaFrame.Post;
+
+	wp.media.view.MediaFrame.Post = wpMediaFramePost.extend(
 	{
-		"use strict";
+		mainInsertToolbar: function( view )
+		{
+			wpMediaFramePost.prototype.mainInsertToolbar.call(this, view);
 
-		console.log(event);
+			var controller = this,
+				toolbar = this.selectionStatusToolbar( view );
 
-		//wpMediaAttachmentFilters.prototype.change.call(this, event);
-	},
+			// console.log(this, this.options, this.state(), this.toolbar, this.state().get('toolbar'), toolbar, controller.state().get('filterable'));
 
-	select: function( event )
+			view.set( "attach", {
+				style: "primary",
+				priority: 80,
+				text: file_gallery.L10n.attach_all_checked_copy,
+				requires: {
+					selection: true
+				},
+
+				click: function()
+				{
+					var state = controller.state(),
+						selection = state.get("selection");
+					
+					jQuery.post
+					(
+						wp.media.model.settings.ajaxurl,
+						{
+							action : "file_gallery_copy_attachments_to_post",
+							post_id : wp.media.model.settings.post.id,
+							ids : _.pluck(selection._byId, "id").join(","),
+							_ajax_nonce : file_gallery_attach_nonce
+						},
+						function(response)
+						{
+							var data = response.split("#"),
+								attached_ids = data[0];
+								response = data[1];
+
+							console.log(response);
+
+							state.reset();
+						},
+						"html"
+					);
+				}
+			});
+		}
+	});
+
+	jQuery(document).on("click", ".insert-media, .media-menu-item", function(e)
 	{
-		"use strict";
+		var attachButton = jQuery(".media-frame-toolbar .media-button-attach"),
+			filters = jQuery("select.attachment-filters");
 
-		console.log("ya");
+		if( filters.val() === "uploaded" ) {
+			attachButton.hide();
+		}
 
-		//wpMediaAttachmentFilters.prototype.select.call(this, event);
-	}
-});/**/
-
-
-var wpMediaFramePost = wp.media.view.MediaFrame.Post;
-wp.media.view.MediaFrame.Post = wpMediaFramePost.extend(
-{
-	mainInsertToolbar: function( view )
-	{
-		"use strict";
-
-		wpMediaFramePost.prototype.mainInsertToolbar.call(this, view);
-
-		var controller = this;
-
-		this.selectionStatusToolbar( view );
-
-		view.set( "attach", {
-			style: "primary",
-			priority: 80,
-			text: file_gallery.L10n.attach_all_checked_copy,
-			requires: { selection: true },
-
-			click: function() {
-				var state = controller.state(),
-					selection = state.get("selection");
-				
-				jQuery.post
-				(
-					wp.media.model.settings.ajaxurl,
-					{
-						action : "file_gallery_copy_attachments_to_post",
-						post_id : wp.media.model.settings.post.id,
-						ids : _.pluck(selection._byId, "id").join(","),
-						_ajax_nonce : file_gallery_attach_nonce
-					},
-					function(response)
-					{
-						var data = response.split("#"),
-							attached_ids = data[0];
-							response = data[1];
-
-						console.log(response);
-
-						state.reset();
-					},
-					"html"
-				);
+		filters.on("change", function()
+		{
+			if( this.value === "uploaded" ) {
+				attachButton.hide();
+			}
+			else {
+				attachButton.show();
 			}
 		});
-	}
+	});
 });
